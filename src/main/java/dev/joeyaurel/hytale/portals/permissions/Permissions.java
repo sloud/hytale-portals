a61efcf4da713085c1cbcc3720c39e2c0e9f35cd
Joey Aurel <159448049+joeyaurel@users.noreply.github.com>
2026-01-22 07:57:46 +0100

    feat(permissions): simplify constants

--- src/main/java/dev/joeyaurel/hytale/portals/permissions/Permissions.java
+++ src/main/java/dev/joeyaurel/hytale/portals/permissions/Permissions.java
@@ -1,24 +1,17 @@
 package dev.joeyaurel.hytale.portals.permissions;
 
 public class Permissions {
-    // Prefix for all permissions
-    private static final String PREFIX = "portals.";
-
     // Admin permission
-    public static final String ADMIN = PREFIX + "admin";
-
-    // Specific prefixes
-    public static final String NETWORK_PREFIX = PREFIX + "network.";
-    public static final String PORTAL_PREFIX = PREFIX + "portal.";
+    public static final String ADMIN = "portals.admin";
 
     // Network permissions
-    public static final String NETWORK_LIST = NETWORK_PREFIX + "list";
-    public static final String NETWORK_CREATE = NETWORK_PREFIX + "create";
-    public static final String NETWORK_DELETE = NETWORK_PREFIX + "delete";
+    public static final String NETWORK_LIST = "portals.network.list";
+    public static final String NETWORK_CREATE = "portals.network.create";
+    public static final String NETWORK_DELETE = "portals.network.delete";
 
     // Portal permissions
-    public static final String PORTAL_LIST = PORTAL_PREFIX + "list";
-    public static final String PORTAL_CREATE = PORTAL_PREFIX + "create";
-    public static final String PORTAL_USE = PORTAL_PREFIX + "use";
-    public static final String PORTAL_DELETE = PORTAL_PREFIX + "delete";
+    public static final String PORTAL_LIST = "portals.portal.list";
+    public static final String PORTAL_CREATE = "portals.portal.create";
+    public static final String PORTAL_USE = "portals.portal.use";
+    public static final String PORTAL_DELETE = "portals.portal.delete";
 }
