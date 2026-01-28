2f03c387c180e55ce46293e645200d1a37de9f40
Joey <159448049+joeyaurel@users.noreply.github.com>
2026-01-28 09:53:40 +0100

    refactor(dependencyinjection): remove unused dependencies
    These dependencies are used when directly called through the PortalsComponent. The methods defined here are only used in the `PortalsPlugin.java` file to initialize the base entry points for dependency injection.

--- src/main/java/dev/joeyaurel/hytale/portals/dependencyinjection/PortalsComponent.java
+++ src/main/java/dev/joeyaurel/hytale/portals/dependencyinjection/PortalsComponent.java
@@ -1,15 +1,7 @@
 package dev.joeyaurel.hytale.portals.dependencyinjection;
 
-import com.hypixel.hytale.logger.HytaleLogger;
-import dev.joeyaurel.hytale.portals.PortalsPlugin;
 import dev.joeyaurel.hytale.portals.commands.PortalCommand;
-import dev.joeyaurel.hytale.portals.commands.portal.PortalCancelCommand;
-import dev.joeyaurel.hytale.portals.commands.portal.PortalCreateCommand;
 import dev.joeyaurel.hytale.portals.database.Database;
-import dev.joeyaurel.hytale.portals.database.repositories.NetworkRepository;
-import dev.joeyaurel.hytale.portals.database.repositories.PortalRepository;
-import dev.joeyaurel.hytale.portals.stores.NetworkStore;
-import dev.joeyaurel.hytale.portals.stores.PortalStore;
 import dev.joeyaurel.hytale.portals.systems.events.BreakBlockEventSystem;
 import dev.joeyaurel.hytale.portals.systems.events.DamageBlockEventSystem;
 import dev.joeyaurel.hytale.portals.systems.tick.EntryTickingSystem;
@@ -19,25 +11,11 @@ import javax.inject.Singleton;
 @Singleton
 @Component(modules = {PortalsModule.class})
 public interface PortalsComponent {
-    // Basics
-    HytaleLogger logger();
-    PortalsPlugin plugin();
-
     // Database
     Database database();
 
-    // Repositories
-    NetworkRepository networkRepository();
-    PortalRepository portalRepository();
-
-    // Stores
-    NetworkStore networkStore();
-    PortalStore portalStore();
-
     // Commands
     PortalCommand portalCommand();
-    PortalCreateCommand portalCreateCommand();
-    PortalCancelCommand portalCancelCommand();
 
     // Systems - Events
     BreakBlockEventSystem breakBlockEventSystem();
